df6d34b0a2838bfdd81b29969d9a80cdb2b7da1e
Ric da Silva <2575243+rmhsilva@users.noreply.github.com>
2020-03-17 17:09:49 +0000

    Add beginnings of constraints def

---
+++ src/c9c/constrain.py
@@ -0,0 +1,17 @@
+"""IAC constraints"""
+
+
+## Properties of infrastructure
+#
+# General
+# - concurrency (max connections/users)
+# - latency (per region)
+# - region
+# - cost (in abstract unit - depends on infra type)
+#
+# Specific
+# - running time (computation only)
+# - amount of disk access required (computation only)
+# -
+
+# operators: all the usual (==, !=, <, >, etc)
