25b9d7b1ffe6f064d0305aefbdc7262d17e94fa6
Ric da Silva <2575243+rmhsilva@users.noreply.github.com>
2020-04-10 17:36:08 +0100

    Make outputs cache global

--- src/c9/synthesiser/outputs.py
+++ src/c9/synthesiser/outputs.py
@@ -19,6 +19,7 @@ OUTPUTS = None
 
 def load_infra_outputs(inf_name: str) -> dict:
     """Load the outputs for the given infrastructure"""
+    global OUTPUTS
     if not OUTPUTS:
         filename = os.path.join(os.getcwd(), OUTPUTS_FILENAME)
         logging.info(f"Loading outputs from {filename}")
