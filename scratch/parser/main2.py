0fed32c99758ae792c28f4d43aa0e8994c6cce14
Ric da Silva <2575243+rmhsilva@users.noreply.github.com>
2020-04-23 13:42:58 +0100

    Implement let and def

--- scratch/parser/main2.py
+++ scratch/parser/main2.py
@@ -41,9 +41,9 @@ EXPR_TESTS = [
     "1",
     '"foo"',
     "symb",
-    "(f, a)",
-    "f a",
-    "f(a, b, c)",
+    "(g, a)",
+    "g a",
+    "g (a, b, c)",
     # -- lists
     "lambda x b: 1",
     """lambda x:
@@ -52,23 +52,29 @@ EXPR_TESTS = [
     "lambda foo: print foo; foo",
     # -- cond
     "if a: 1 else: 2",
-    """if a:
-    print bla
-    print bla; print bla
-    bla
-else: 2""",
-    # -- bindings
-    """let a = 1 in: a""",
-    """let a = 1
-    b = 2
-in: a""",
 ]
 
-# foo = """
-# foo x, y:
-#     let a = b, bla = bla:
-#         x + a
-# """
+BODY_TESTS = [
+    """
+def f:
+    if a:
+        print bla
+        print bla; print bla
+        bla
+    else: 2
+""",
+    # -- bindings
+    """def f x: let a = 1 in a + x""",
+    """
+def f:
+    let a = 1
+        b = 2
+    in a
+    """,
+    # -- func
+    "def f x: 1 + f (x - 1) * 3",
+]
+
 
 # CLOSURE = """
 # make_counter = let val = 0: lambda: val = val + 1; val
@@ -86,18 +92,27 @@ class C9Indenter(Indenter):
     tab_len = 4  # replaces tabs with this many spaces
 
 
-def main(make_tree=False):
-    # https://github.com/lark-parser/lark/blob/master/examples/python_parser.py
-    parser = lark.Lark.open("c9_func.lark", parser="lalr", postlex=C9Indenter())
-
-    for i, t in enumerate(EXPR_TESTS):
-        print(f"-[{i}]------------------------------------------------------")
-        print(t)
-        print(":")
+def test(tests, start, make_tree):
+    parser = lark.Lark.open(
+        "c9_func.lark", parser="lalr", start=start, postlex=C9Indenter()
+    )
+    for i, t in enumerate(tests):
+        print(f"------------------------------------------------------[{start}_{i}]-")
+        print("\n".join(f"{i+1} | {l}" for i, l in enumerate(t.split("\n"))))
+        print("  :")
         tree = parser.parse(t)
         if make_tree:
-            lark.tree.pydot__tree_to_png(tree, f"tree{i}.png")
+            lark.tree.pydot__tree_to_png(tree, f"tree_{start}_{i}.png")
         print(tree.pretty())
+
+
+def main(make_tree=False):
+    # https://github.com/lark-parser/lark/blob/master/examples/python_parser.py
+
+    body_tests = [t.strip() + "\n" for t in BODY_TESTS]
+
+    test(EXPR_TESTS, "exp", make_tree)
+    test(body_tests, "body", make_tree)
 
 
 if __name__ == "__main__":
