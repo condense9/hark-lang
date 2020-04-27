e2d206266d508e43d079e86b5858ce12da31047d
Ric da Silva <2575243+rmhsilva@users.noreply.github.com>
2020-04-27 11:13:34 +0100

    Add type serialisation

--- src/c9/machine/types.py
+++ src/c9/machine/types.py
@@ -1,76 +1,129 @@
 """Primitives data types"""
 
+from collections import UserList
+
 
 class C9Type:
     """Base class"""
 
-    def serialise(self):
-        return "foo"
+    def serialise(self) -> list:
+        return [type(self).__name__]
+
+    @classmethod
+    def deserialise(cls, obj: list):
+        name = obj[0]
+        new_cls = globals()[name]
+
+        if len(obj) == 1:
+            return new_cls()
+        elif len(obj) == 2:
+            data = obj[1]
+
+            if issubclass(new_cls, C9Compound):
+                data = [cls.deserialise(a) for a in data]
+
+            return new_cls(data)
+        else:
+            raise ValueError(obj)
+
+    def __repr__(self):
+        return f"<{type(self).__name__}>"
+
+    def __eq__(self, other):
+        return type(self) == type(other)
+
+
+### Atomics
 
 
 class C9Atomic(C9Type):
-    """Atomic (single-value) types"""
+    """Atomic (singleton) types"""
 
 
-class C9Symbol(C9Type, str):
-    pass
+class C9True(C9Atomic):
+    """Represent True"""
 
 
-class C9Quote(C9Atomic):
-    def __init__(self, val):
-        self.val = val
+class C9Null(C9Atomic):
+    """Represent Null (False)"""
+
+
+### Literals
+
+
+class C9Literal(C9Type):
+    """A literal value which has an underlying Python type"""
+
+    def __init__(self, data):
+        self.data = data
+
+    def serialise(self) -> list:
+        return super().serialise() + [self.data]
 
     def __repr__(self):
-        return f"<Quote {self.val}>"
+        sup = super().__repr__()
+        return f"<{sup} {self.data}>"
+
+    def __eq__(self, other):
+        return super().__eq__(other) and self.data == other.data
 
 
-class C9Float(C9Atomic, float):
+class C9Symbol(str, C9Literal):
     pass
 
 
-class C9Int(C9Atomic, int):
+class C9Float(float, C9Literal):
     pass
 
 
-class C9String(C9Atomic, str):
+class C9Int(int, C9Literal):
     pass
 
 
-class C9Bool(C9Atomic, str):
+class C9String(str, C9Literal):
     pass
 
 
-class C9Function(C9Atomic, str):
+class C9Function(str, C9Literal):
     """A function defined in C9"""
 
 
-class C9Foreign(C9Atomic, str):
+class C9Foreign(str, C9Literal):
     """A foreign function"""
 
 
-class C9Instruction(C9Atomic, str):
+class C9Instruction(str, C9Literal):
     """A C9 machine instruction"""
 
 
-class C9True(C9Bool, str):
-    def __repr__(self):
-        return "True"
-
-
-class C9Null(C9Atomic, str):
-    """Singleton to represent Null"""
-
-    def __repr__(self):
-        return "nil"
+### Compound types
 
 
 class C9Compound(C9Type):
-    """Structured types"""
+    def __init__(self, data):
+        self.data = data
+
+    def serialise(self) -> list:
+        data = [a.serialise() for a in self.data]
+        return super().serialise() + [data]
+
+    def __repr__(self):
+        sup = super().__repr__()
+        return f"<{sup} {self.data}>"
+
+    def __eq__(self, other):
+        return super().__eq__(other) and self.data == other.data
 
 
-class C9List(C9Compound, list):
-    pass
+class C9Quote(C9Compound):
+    """A quoted value"""
 
 
-class C9Dict(C9Compound, dict):
-    pass
+class C9List(UserList, C9Compound):
+    def __init__(self, data):
+        super(C9List, self).__init__(data)
+
+
+# TODO:
+# class C9Dict(C9Compound, dict):
+#     pass
---
+++ test/test_types.py
@@ -0,0 +1,44 @@
+import pytest
+
+from c9.machine.types import *
+
+
+def test_true():
+    original = C9True()
+    ser = original.serialise()
+    assert isinstance(ser, list)
+    assert isinstance(ser[0], str)
+    deser = C9Type.deserialise(ser)
+    assert original == deser
+
+
+def test_literals():
+    int_a = C9Int(5)
+    int_b = C9Int(3)
+    int_c = C9Int(4)
+    assert isinstance(int_a, int)
+    assert int_a + int_b == 8
+    assert hasattr(int_a, "data")
+    ser = int_a.serialise()
+    deser = C9Type.deserialise(ser)
+    assert deser + int_c == 9
+
+
+def test_list():
+    list_a = C9List([C9Int(1), C9Int(2), C9Int(3)])
+    assert len(list_a) == 3
+    list_a.append(C9Int(789))
+    assert len(list_a) == 4
+    assert hasattr(list_a, "data")
+    ser = list_a.serialise()
+    deser = C9Type.deserialise(ser)
+    print(deser)
+    assert deser[0] == C9Int(1)
+    assert deser[3] == C9Int(789)
+
+
+def test_quote():
+    obj = C9Quote(C9List([C9Int(1), C9String("foo")]))
+    ser = obj.serialise()
+    deser = C9Type.deserialise(ser)
+    assert deser == obj
