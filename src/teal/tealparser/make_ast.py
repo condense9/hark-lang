851cecbace4f336bd5d7a1bc01c3899a7852b61d
Ric da Silva <2575243+rmhsilva@users.noreply.github.com>
2020-04-30 13:44:46 +0100

    Add logging to make_ast

--- src/teal/tealparser/make_ast.py
+++ src/teal/tealparser/make_ast.py
@@ -1,8 +1,11 @@
 """Make PNG AST graphs"""
 
+import logging
 import lark
 from .load import file_parser
 from .read import ReadLiterals
+
+LOG = logging.getLogger(__name__)
 
 
 def make_ast(filename, function, dest_png):
@@ -12,6 +15,13 @@ def make_ast(filename, function, dest_png):
     parser = file_parser()
     tree = parser.parse(content)
     ast = ReadLiterals().transform(tree)
+
+    LOG.info(f"Creating PNG for function `{function}` in {filename}")
+
     for c in ast.children:
         if c.data == "def_" and c.children[0] == function:
+            LOG.info(f"-> {dest_png}")
             lark.tree.pydot__tree_to_png(c, dest_png)
+            return
+
+    raise ValueError(f"Could not find function `{function}`.")
