ef7867e18b9676e322731e0d2c8e38a452e1e258
Ric da Silva <2575243+rmhsilva@users.noreply.github.com>
2020-06-24 09:59:47 +0100

    Set default region to eu-west-2 for local dynamodb server

--- scripts/run_dynamodb_local.sh
+++ scripts/run_dynamodb_local.sh
@@ -9,6 +9,7 @@ set -e
 PORT=9000
 TABLE=TealSessions
 
+AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-eu-west-2}
 
 CID=$(docker run --rm -d -p ${PORT}:8000 amazon/dynamodb-local)
 
@@ -23,9 +24,11 @@ trap 'quit' SIGINT
 if [[ -n "$1" ]]; then
     aws dynamodb create-table --table-name ${TABLE} \
         --attribute-definitions AttributeName=session_id,AttributeType=S \
+        AttributeName=item_id,AttributeType=S \
         --key-schema AttributeName=session_id,KeyType=HASH \
+        AttributeName=item_id,KeyType=RANGE \
         --billing-mode PAY_PER_REQUEST \
-        --endpoint-url http://localhost:${PORT} >/dev/null
+        --endpoint-url http://localhost:${PORT}
 fi
 
 set +x
